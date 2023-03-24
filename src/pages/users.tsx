0116454b58d29b4dbcca0d3aa64e14988c84ac9b
takeyu1013 <yuto1013@gmail.com>
2023-03-25 00:52:29 +0900

    🚧

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,10 +1,89 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import type { FC } from "react";
 import { Suspense } from "react";
 
 import { api } from "../utils/api";
+
+const Pagination: FC<{ total: number; current: number }> = ({
+  total,
+  current,
+}) => {
+  const className =
+    "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0";
+
+  return (
+    <nav className="inline-flex -space-x-px py-10" aria-label="Pagination">
+      <Link
+        href="#"
+        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      >
+        ← Previous
+      </Link>
+      {total < 7 ? (
+        [...Array<undefined>(total)].map((_, index) => {
+          const page = index + 1;
+          return (
+            <Link
+              key={index}
+              href={{ pathname: "/users", query: { page } }}
+              aria-current="page"
+              className={
+                page === current
+                  ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  : className
+              }
+            >
+              {page}
+            </Link>
+          );
+        })
+      ) : (
+        <>
+          {[...Array<undefined>(5)].map((_, index) => {
+            const page = index + 1;
+            return (
+              <Link
+                key={index}
+                href={{ pathname: "/users", query: { page } }}
+                aria-current="page"
+                className={
+                  page === current
+                    ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    : className
+                }
+              >
+                {page}
+              </Link>
+            );
+          })}
+          <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+            ...
+          </span>
+          <Link
+            href={{ pathname: "/users", query: { page: total } }}
+            aria-current="page"
+            className={
+              total === current
+                ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                : className
+            }
+          >
+            {total}
+          </Link>
+        </>
+      )}
+      <Link
+        href="#"
+        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      >
+        Next →
+      </Link>
+    </nav>
+  );
+};
 
 const Users: FC = () => {
   const {
@@ -42,44 +121,21 @@ const Users: FC = () => {
 };
 
 const UsersPage: NextPage = () => {
+  const {
+    query: { page },
+  } = useRouter();
+  const current = Number.isNaN(Number(page)) ? 1 : Number(page);
+
   return (
     <>
       <h1 className="pt-5 pb-[30px] text-center text-[42px] leading-none tracking-[-2px]">
         All users
       </h1>
-      <nav className="inline-flex -space-x-px py-10" aria-label="Pagination">
-        <Link
-          href="#"
-          className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-        >
-          ← Previous
-        </Link>
-        {[...Array<undefined>(10)].map((_, index) => {
-          const page = index + 1;
-          return (
-            <Link
-              key={index}
-              href={{ pathname: "/users", query: { page } }}
-              aria-current="page"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              {page}
-            </Link>
-          );
-        })}
-        {/* <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-          ...
-        </span> */}
-        <Link
-          href="#"
-          className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-        >
-          Next →
-        </Link>
-      </nav>
+      <Pagination total={10} current={current} />
       <Suspense fallback={<div>Loading...</div>}>
         <Users />
       </Suspense>
+      <Pagination total={10} current={current} />
     </>
   );
 };
