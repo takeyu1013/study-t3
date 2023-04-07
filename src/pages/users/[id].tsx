cd2956a87ed0925e83a44471381a6f59d13391c5
takeyu1013 <yuto1013@gmail.com>
2023-04-08 01:12:20 +0900

    🤡

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import type { FC } from "react";
 import { Suspense } from "react";
@@ -24,7 +25,7 @@ const UserData: FC<{ userId: string }> = ({ userId }) => {
   );
 
   return (
-    <div className="flex gap-[10px] pt-[30px] pb-[10px]">
+    <div className="flex gap-[10px] pt-[20px] pb-[10px]">
       {image && <Image alt="avatar" src={image} width={80} height={80} />}
       <h1 className="text-[19.6px] tracking-[-1px] text-[#333333]">{name}</h1>
     </div>
@@ -50,11 +51,57 @@ const Fallback: FC<FallbackProps> = ({ error: { message } }) => {
 
 const Show: NextPage = () => {
   return (
-    <ErrorBoundary FallbackComponent={Fallback}>
-      <Suspense fallback={<div>Loading user data...</div>}>
-        <User />
-      </Suspense>
-    </ErrorBoundary>
+    <main className="flex gap-[30px]">
+      <aside className="max-w-[360px] grow">
+        <ErrorBoundary FallbackComponent={Fallback}>
+          <Suspense fallback={<div>Loading user data...</div>}>
+            <User />
+          </Suspense>
+        </ErrorBoundary>
+      </aside>
+      <div className="grow">
+        <h3 className="pt-5 pb-[10px] text-[24px] font-medium">
+          Microposts (50)
+        </h3>
+        <ol>
+          {[...Array<undefined>(50)].map((_, index) => {
+            return (
+              <li
+                key={index}
+                className="flex items-center gap-[10px] border-t-[1px] border-[#e8e8e8] py-[10px]"
+              >
+                <Image
+                  src={
+                    "https://secure.gravatar.com/avatar/bebfcf57d6d8277d806a9ef3385c078d?s=50"
+                  }
+                  alt="avatar"
+                  width={50}
+                  height={50}
+                  className="h-full"
+                />
+                <div className="leading-none">
+                  <Link
+                    href="/users/clf5aigzy0000ubx5mk8cywov"
+                    className="text-[14px] leading-5 text-[#337ab7] hover:text-[#23527c] hover:underline"
+                  >
+                    Yuto Takeuchi
+                  </Link>
+                  <p className="text-[14px] leading-5">
+                    Similique eos consequatur nesciunt reiciendis.
+                  </p>
+                  <span className="text-[14px] leading-5 text-[#777777]">
+                    Posted 3 minites ago.{" "}
+                  </span>
+                  <button className="text-[14px] leading-5 text-[#337ab7] hover:text-[#23527c] hover:underline">
+                    delete
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </main>
   );
 };
 
