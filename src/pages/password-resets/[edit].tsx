565833f60eeed2b6426dde0dc19f9ec6db156b06
takeyu1013 <yuto1013@gmail.com>
2023-04-22 16:54:25 +0900

    💄

--- src/pages/password-resets/[edit].tsx
+++ src/pages/password-resets/[edit].tsx
@@ -36,24 +36,58 @@ const Edit: NextPage = () => {
   });
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        if (typeof token !== "string" || typeof email !== "string") {
-          return;
-        }
-        mutate({ email, token, ...data });
-      })}
-    >
-      <label>Password</label>
-      <input {...register("password")} type="password" />
-      {errors.password?.message && <p>{errors.password.message}</p>}
-      <label>Confirmation</label>
-      <input {...register("passwordConfirmation")} type="password" />
-      {errors.passwordConfirmation?.message && (
-        <p>{errors.passwordConfirmation.message}</p>
-      )}
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <h1 className="color-[#333333] pt-[20px] pb-[30px] text-center text-[42px] leading-[42px] tracking-[-2px]">
+        Reset password
+      </h1>
+      <div className="flex justify-center">
+        <form
+          className="max-w-[555px] grow"
+          onSubmit={handleSubmit((data) => {
+            if (typeof token !== "string" || typeof email !== "string") {
+              return;
+            }
+            mutate({ email, token, ...data });
+          })}
+        >
+          <div className="pb-[5px]">
+            <label className="color-[#333333] h-5 text-[14px] font-bold leading-5">
+              Password
+            </label>
+          </div>
+          <div className="pb-[15px]">
+            <input
+              className="w-full rounded border border-[#cccccc] py-[6px] px-3 text-[14px]"
+              {...register("password")}
+              type="password"
+            />
+          </div>
+          {errors.password?.message && <p>{errors.password.message}</p>}
+          <div className="pb-[5px]">
+            <label className="color-[#333333] h-5 text-[14px] font-bold leading-5">
+              Confirmation
+            </label>
+          </div>
+          <div className="pb-[15px]">
+            <input
+              className="w-full rounded border border-[#cccccc] py-[6px] px-3 text-[14px]"
+              {...register("passwordConfirmation")}
+              type="password"
+            />
+
+            {errors.passwordConfirmation?.message && (
+              <p>{errors.passwordConfirmation.message}</p>
+            )}
+          </div>
+          <button
+            className="w-full rounded border border-[#2e6da4] bg-[#337ab7] py-[6px] px-3 text-[14px] text-white hover:bg-[#286090]"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
